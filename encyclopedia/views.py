a84165ca0e65c708a86a05b155742753c7586557
Hanxu Liang <sam.liang.wk@gmail.com>
2020-09-27 06:12:19 -0400

    Initial commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def add(request):
+	return render(request,"encyclopedia/add.html")
+
+def entry(request,title):
+	entry = util.get_entry(title)
+	return render(request, "encyclopedia/entry.html", {
+		"title": title,
+		"entry": entry
+		})
